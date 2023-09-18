a070da427eabf2d136e565a32fcdea8d8efd6c9c
frankMarcelino <frank.silva.ti@gmail.com>
2023-09-18 19:35:55 -0300

    fim do projeto hora do dia

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ function carregar() {
     var img = window.document.getElementById('iimagem')
     var data = new Date()
     var hora = data.getHours()
-
+    
     msg.innerHTML = `Agora são ${hora} Horas!`
     if (hora >= 0 && hora < 12){
         // bom dia
         img.src = 'manha.png'
+        document.body.style.background = '#e2cd9f'
     } else if (hora >= 12 && hora < 18 ) {
         // Boa tarde
         img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
     } else {
         // boa noite
         img.src = 'noite.png'
+        document.body.style.background = '#515154'
     }
 }
 
