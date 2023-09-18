02ba69a997d19293e0a2c686c0b5c9c600bb5174
frankMarcelino <frank.silva.ti@gmail.com>
2023-09-18 19:31:09 -0300

    finalizando projeto

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-alert('ola!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('iimagem')
+    var data = new Date()
+    var hora = data.getHours()
+
+    msg.innerHTML = `Agora são ${hora} Horas!`
+    if (hora >= 0 && hora < 12){
+        // bom dia
+        img.src = 'manha.png'
+    } else if (hora >= 12 && hora < 18 ) {
+        // Boa tarde
+        img.src = 'tarde.png'
+    } else {
+        // boa noite
+        img.src = 'noite.png'
+    }
+}
+
+
